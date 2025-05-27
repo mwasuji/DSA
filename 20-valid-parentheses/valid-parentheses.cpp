b4cc6d79545eec45b12274dcc3a5d82691944359
mwasuji <131579062+mwasuji@users.noreply.github.com>
2025-05-28 00:36:51 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (34.98%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -3,24 +3,23 @@ public:
     bool isValid(string s) {
         stack<char> st;
         
-        for (char ch : s) {
-            if (ch == '(' || ch == '{' || ch == '[') {
+       for (char ch : s ){
+          if (ch == '(' || ch == '{' || ch == '[') {
                 st.push(ch);
-            } else {
-                // Ensure the stack is not empty before accessing top
-                if (st.empty()) return false;
-                
-                if ((ch == ')' && st.top() == '(') ||
+        }
+        else{
+            if( st.empty() ) return false;
+
+            if ((ch == ')' && st.top() == '(') ||
                     (ch == '}' && st.top() == '{') ||
                     (ch == ']' && st.top() == '[')) {
                     st.pop();
-                } else {
-                    return false; // Mismatched parentheses
-                }
-            }
+        }else{
+            return false;
         }
-        
-        // Stack should be empty if all parentheses are valid
+       
+       }
+       }
         return st.empty();
     }
 };
