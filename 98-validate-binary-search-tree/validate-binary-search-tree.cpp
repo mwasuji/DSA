964d4fac06361ff963bea1eaa33bdc4385931ad7
mwasuji <131579062+mwasuji@users.noreply.github.com>
2025-07-26 15:07:10 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (94.80%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -1,42 +1,17 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    bool isValidBST(TreeNode* root) {
-        if(!root) return true;
-        bool isBST = true;
-        TreeNode* cur = root;
-        long prev = (long)INT_MIN-1;
-        while(cur){
-            if(cur->left == NULL){
-                if(cur->val <= prev) isBST = false;
-                prev = cur->val;
-                cur = cur->right;
-            }
-            else{
-                TreeNode* temp = cur->left;
-                while(temp->right && temp->right != cur) temp = temp->right;
-                if(temp->right == NULL){
-                    temp->right = cur;
-                    cur = cur->left;
-                }
-                else{
-                    temp->right = NULL;
-                    if(cur->val <= prev) isBST = false;
-                    prev = cur->val;
-                    cur = cur->right;
-                }
-            }
+    bool isbst(TreeNode* root, long min, long max) {
+        if (root == NULL) return true;
+
+        if (root->val > min && root->val < max) {
+            return isbst(root->left, min, root->val) &&
+                   isbst(root->right, root->val, max);
+        } else {
+            return false;
         }
-        return isBST;
+    }
+
+    bool isValidBST(TreeNode* root) {
+        return isbst(root, LONG_MIN, LONG_MAX);
     }
 };
