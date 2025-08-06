e84b82670eda46489ce6bfeb3821f15baaa0ced0
mwasuji <131579062+mwasuji@users.noreply.github.com>
2025-08-06 23:40:18 +0530

    Time: 0 ms (100.00%) | Memory: 112.2 MB (94.30%) - LeetSync

--- 134-gas-station/gas-station.cpp
+++ 134-gas-station/gas-station.cpp
@@ -1,33 +1,39 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int start = 0;
-        int deficit = 0;
-        int balance = 0;
+        int start=0;
+        int deficit=0;
+        int balance=0;
 
-        for(int i=0; i<gas.size(); i++) {
-            if(balance + gas[i] < cost[i]) {
-                //deficit case
-                deficit += balance+gas[i]-cost[i];
-                //deficit case -> aage nahi ja skta , start update
-                start = i+1;
-                //agar deficit exist krta h , toh common sense hai
-                //k balance me koi bhi gas nhi hogi
-                //that swhy balance gain zero kardo
-                balance = 0;
+        // for(int i=0; i<gas.size(); i++){
+        //     balance += gas[i] - cost[i];
+        //     //+ve balance -> nothing to worry about 
+        //     //-ve balance -> ye deficit ko darshata hai 
+        //     if(balance<0){
+        //         deficit += abs(balance);
+        //         start = i+1;
+        //         balance =0;
+
+        //     }
+        // }
+        // if(balance - deficit >=0){
+        //     return start;
+        // }
+        // return -1;
+
+        for(int i=0; i<gas.size(); i++){
+            if(balance + gas[i] < cost[i]){
+                deficit += abs(balance + gas[i] - cost[i]);
+                start =i+1;
+                balance =0;
             }
-            else {
-                //extra gas bach gyi h, balance me add krdo 
-                balance = balance + gas[i]-cost[i];
+            else{
+                balance += gas[i] - cost[i];
             }
         }
-        //deficit -> -ve value
-        //balance -> +ve value
-        //>= 0 -> 0 or extra gas bach jaegi overall -> circle complete
-
-        if(balance + deficit >= 0) 
+        if(balance - deficit >=0){
             return start;
-        else
-            return -1;
+        }
+        return -1;
     }
 };
